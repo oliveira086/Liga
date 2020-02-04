b72f8940cc5c5abefc1316775f95deed6963fe09
Andre Oliveira <andreluisoliveira013@gmail.com>
2020-02-04 16:52:51 -0300

    upando objeto

--- objeto.py
+++ objeto.py
@@ -7,11 +7,12 @@ bot = telepot.Bot('552538744:AAE5r1s7wRAHrdxHq6xYWUcRLnfVgar3xQo')
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
 
+    print(msg)
+
 
 MessageLoop(bot, handle).run_as_thread()
 print ('Online')
 
-# Keep the program running.
 while 1:
     time.sleep(10)
 	
