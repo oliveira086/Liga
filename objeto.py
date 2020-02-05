17aeccc2b2c15b653421eb65e964dea620bf48d7
Andre Oliveira <andreluisoliveira013@gmail.com>
2020-02-04 21:36:18 -0300

    adicionando mais ativos

--- objeto.py
+++ objeto.py
@@ -20,26 +20,46 @@ def handle(msg):
             'posicao': [90, 350],
             'porcetagem': mensagemTratada[10],
             'posicaoPorcentagem': [90, 405],
+            'cor':(0,255,0)
         },
         {
             'ativo': mensagemTratada[1],
             'posicao': [310, 350],
             'porcetagem': mensagemTratada[11],
             'posicaoPorcentagem': [310, 405],
+            'cor':(0,255,0)
+        },
+
+         {
+            'ativo': mensagemTratada[2],
+            'posicao': [535, 350],
+            'porcetagem': mensagemTratada[12],
+            'posicaoPorcentagem': [535, 405],
+            'cor':(0,255,0)
+        },
+        {
+            'ativo': mensagemTratada[3],
+            'posicao': [200, 520],
+            'porcetagem': mensagemTratada[13],
+            'posicaoPorcentagem': [200, 575],
+            'cor':(0,255,0)
         },
         ]
 
         
-
-        for i in range(1):
-            imagem = Image.open('fundo.jpg')
+        imagem = Image.open('fundo.jpg')
+        imagem.save('pronto.jpg')
+        
+        for i in range(4):
+            imagem = Image.open('pronto.jpg')
             draw = ImageDraw.Draw(imagem)
             font = ImageFont.truetype("arial.ttf", 30)
 
             draw.text((lista[i]['posicao'][0],lista[i]['posicao'][1]), '{}'.format(lista[i]['ativo']), (12, 12, 12), font=font)
-            draw.text((lista[i]['posicaoPorcentagem'][0], lista[i]['posicaoPorcentagem'][1]), '{}'.format(lista[i]['porcetagem']), (0, 255, 0), font=font)
+            draw.text((lista[i]['posicaoPorcentagem'][0], lista[i]['posicaoPorcentagem'][1]), '{}'.format(lista[i]['porcetagem']), lista[i]['cor'], font=font)
 
-        imagem.save('pronto.jpg')
+            imagem.save('pronto.jpg')
+        
 
 
 MessageLoop(bot, handle).run_as_thread()
