254b86cdaaf7e26009c0f2ba2a6e66c1d24cbb6e
Andre Oliveira <andreluisoliveira013@gmail.com>
2020-02-04 21:43:51 -0300

    funcao de escrever ativo e porcentagem concluida

--- objeto.py
+++ objeto.py
@@ -44,13 +44,55 @@ def handle(msg):
             'posicaoPorcentagem': [200, 575],
             'cor':(0,255,0)
         },
+        {
+            'ativo': mensagemTratada[4],
+            'posicao': [430, 520],
+            'porcetagem': mensagemTratada[14],
+            'posicaoPorcentagem': [420, 575],
+            'cor':(0,255,0)
+        },
+        {
+            'ativo': mensagemTratada[5],
+            'posicao': [95, 840],
+            'porcetagem': mensagemTratada[15],
+            'posicaoPorcentagem': [80, 900],
+            'cor':(255,0,0)
+        },
+        {
+            'ativo': mensagemTratada[6],
+            'posicao': [310, 840],
+            'porcetagem': mensagemTratada[16],
+            'posicaoPorcentagem': [310, 900],
+            'cor':(255,0,0)
+        },
+        {
+            'ativo': mensagemTratada[7],
+            'posicao': [535, 840],
+            'porcetagem': mensagemTratada[17],
+            'posicaoPorcentagem': [530, 900],
+            'cor':(255,0,0)
+        },
+        {
+            'ativo': mensagemTratada[8],
+            'posicao': [200, 1010],
+            'porcetagem': mensagemTratada[18],
+            'posicaoPorcentagem': [200, 1070],
+            'cor':(255,0,0)
+        },
+         {
+            'ativo': mensagemTratada[9],
+            'posicao': [430, 1010],
+            'porcetagem': mensagemTratada[19],
+            'posicaoPorcentagem': [420, 1070],
+            'cor':(255,0,0)
+        },
         ]
 
         
         imagem = Image.open('fundo.jpg')
         imagem.save('pronto.jpg')
-        
-        for i in range(4):
+
+        for i in range(10):
             imagem = Image.open('pronto.jpg')
             draw = ImageDraw.Draw(imagem)
             font = ImageFont.truetype("arial.ttf", 30)
